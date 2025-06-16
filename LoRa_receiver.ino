c16af7c39a304dd4154af9ea048cddd7a079c874
dvsmihir1 <bl.en.u4cse22110@bl.students.amrita.edu>
2025-06-16 22:47:51 +0530

    Add files via upload

---
+++ LoRa_receiver.ino
@@ -0,0 +1,24 @@
+#include <ESP8266.h>
+#include <LoRa.h>
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  if (!LoRa.begin(433E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  Serial.println("LoRa Receiver Started...");
+}
+
+void loop() {
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    Serial.print("Received packet: ");
+    while (LoRa.available()) {
+      Serial.print((char)LoRa.read());
+    }
+    Serial.println();
+  }
+}

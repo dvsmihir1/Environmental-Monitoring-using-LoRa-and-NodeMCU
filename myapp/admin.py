78b23c56bf1818cd522b62dc72c59b2d1cfc67ce
dvsmihir1 <bl.en.u4cse22110@bl.students.amrita.edu>
2025-06-16 22:51:24 +0530

    Delete myapp directory

--- myapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Result
-from .models import Feedback
-
-admin.site.register(Result)
-admin.site.register(Feedback)
--- myapp/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class FeedbackForm(forms.Form):
-    feedback_text = forms.CharField(widget=forms.Textarea(attrs={'rows': 3}))
